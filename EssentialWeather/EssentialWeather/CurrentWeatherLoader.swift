b7736c4e95fa5ee8b2829238ac12765ff773ea84
Karlis Berzins <berzinsh.karlis@gmail.com>
2020-01-16 07:05:11 +0200

    Create the CurrentWeatherLoader protocol boundary

---
+++ EssentialWeather/EssentialWeather/CurrentWeatherLoader.swift
@@ -0,0 +1,15 @@
+//
+//  CurrentWeatherLoader.swift
+//  EssentialWeather
+//
+//  Created by karlis.berzins on 16/01/2020.
+//  Copyright © 2020 karlis.berzins. All rights reserved.
+//
+
+import Foundation
+
+typealias LoadCurrentWeatherResult = Swift.Result<Weather, Error>
+
+protocol CurrentWeatherLoader {
+    func load(completion: @escaping (LoadCurrentWeatherResult) -> Void)
+}
