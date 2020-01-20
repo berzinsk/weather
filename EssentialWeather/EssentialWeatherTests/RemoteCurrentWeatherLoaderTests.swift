cd2b51e67845a51be95bab7d26d2cb8f9d5d6c4c
Karlis Berzins <berzinsh.karlis@gmail.com>
2020-01-20 07:45:51 +0200

    Request data from URL on 'load'

--- EssentialWeather/EssentialWeatherTests/RemoteCurrentWeatherLoaderTests.swift
+++ EssentialWeather/EssentialWeatherTests/RemoteCurrentWeatherLoaderTests.swift
@@ -9,18 +9,42 @@
 import XCTest
 
 class RemoteCurrentWeatherLoader {
+    private let client: HTTPClient
 
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    func load() {
+        client.get(from: URL(string: "http://a-url.com")!)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 class RemoteCurrentWeatherLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        let _ = RemoteCurrentWeatherLoader()
+        let client = HTTPClientSpy()
+        _ = RemoteCurrentWeatherLoader(client: client)
 
         XCTAssertNil(client.requestedURL)
+    }
+
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = RemoteCurrentWeatherLoader(client: client)
+
+        sut.load()
+
+        XCTAssertNotNil(client.requestedURL)
     }
 }
