aefb3ae4367c84e72ab9551d803fcc7f89be20f2
Karlis Berzins <berzinsh.karlis@gmail.com>
2020-01-24 15:28:31 +0200

    Remove duplicate test code by creating reusable 'expect' helper method

--- EssentialWeather/EssentialWeatherTests/RemoteCurrentWeatherLoaderTests.swift
+++ EssentialWeather/EssentialWeatherTests/RemoteCurrentWeatherLoaderTests.swift
@@ -38,13 +38,10 @@ class RemoteCurrentWeatherLoaderTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
 
-        var capturedError: [RemoteCurrentWeatherLoader.Error] = []
-        sut.load { capturedError.append($0) }
-
-        let clientError = NSError(domain: "Test", code: 0)
-        client.complete(with: clientError)
-
-        XCTAssertEqual(capturedError, [.connectivity])
+        expect(sut, toCompleteWithError: .connectivity, when: {
+            let clientError = NSError(domain: "Test", code: 0)
+            client.complete(with: clientError)
+        })
     }
 
     func test_load_deliversErrorOnNon200HTTPResponse() {
@@ -52,25 +49,19 @@ class RemoteCurrentWeatherLoaderTests: XCTestCase {
 
         let samples = [199, 201, 300, 400, 500].enumerated()
         samples.forEach { index, code in
-            var capturedErrors: [RemoteCurrentWeatherLoader.Error] = []
-            sut.load { capturedErrors.append($0) }
-
-            client.complete(withStatusCode: code, at: index)
-
-            XCTAssertEqual(capturedErrors, [.invalidData])
+            expect(sut, toCompleteWithError: .invalidData, when: {
+                client.complete(withStatusCode: code, at: index)
+            })
         }
     }
 
     func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
         let (sut, client) = makeSUT()
 
-        var capturedErrors: [RemoteCurrentWeatherLoader.Error] = []
-        sut.load { capturedErrors.append($0) }
-
-        let invalidJSON = Data("invalid json".utf8)
-        client.complete(withStatusCode: 200, data: invalidJSON)
-
-        XCTAssertEqual(capturedErrors, [.invalidData])
+        expect(sut, toCompleteWithError: .invalidData, when: {
+            let invalidJSON = Data("invalid json".utf8)
+            client.complete(withStatusCode: 200, data: invalidJSON)
+        })
     }
 
     // MARK:- Helpers
@@ -80,6 +71,15 @@ class RemoteCurrentWeatherLoaderTests: XCTestCase {
         let sut = RemoteCurrentWeatherLoader(url: url, client: client)
 
         return (sut, client)
+    }
+
+    private func expect(_ sut: RemoteCurrentWeatherLoader, toCompleteWithError error: RemoteCurrentWeatherLoader.Error, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        var capturedErrors: [RemoteCurrentWeatherLoader.Error] = []
+        sut.load { capturedErrors.append($0) }
+
+        action()
+
+        XCTAssertEqual(capturedErrors, [error], file: file, line: line)
     }
 
     private class HTTPClientSpy: HTTPClient {
